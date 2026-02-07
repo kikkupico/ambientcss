b6560cc38da966785d4abfc5f8791dc236556a96
Ram <kikkupico@gmail.com>
2026-02-07 03:03:50 +0100

    improvs

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ document
   .getElementById("button-day")
   .addEventListener("click", async function setDay() {
     animateVar("--amb-light-x", (val) => val > -1, -1 / 60);
-    animateVar("--amb-key-light-intensity", (val) => val < 0.9, 0.3 / 60);
-    animateVar("--amb-fill-light-intensity", (val) => val < 0.7, 0.3 / 60);
+    animateVar("--amb-key-light-intensity", (val) => val < 0.9, 0.6 / 60);
+    animateVar("--amb-fill-light-intensity", (val) => val < 0.7, 0.6 / 60);
   });
 document
   .getElementById("button-night")
   .addEventListener("click", async function setNight() {
     animateVar("--amb-light-x", (val) => val < 1, 1 / 60);
-    animateVar("--amb-key-light-intensity", (val) => val > 0.3, -0.3 / 60);
-    animateVar("--amb-fill-light-intensity", (val) => val > 0.1, -0.3 / 60);
+    animateVar("--amb-key-light-intensity", (val) => val > 0.3, -0.6 / 60);
+    animateVar("--amb-fill-light-intensity", (val) => val > 0.1, -0.6 / 60);
   });
